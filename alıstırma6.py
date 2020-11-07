f07adf5c1540a6cc8b7f36bd1f964adf52ba4b59
EzgiUysal <57458821+EzgiUysal@users.noreply.github.com>
2020-11-07 16:10:45 +0300

    Add files via upload

---
+++ alıstırma6.py
@@ -0,0 +1,9 @@
+ezgi = 0
+for a in range(1000,10000,1):
+    a = str(a)
+    if a[0]>a[3]:
+        ezgi+=1
+        
+print(ezgi)
+    
+    
---
+++ palindromik4.py
@@ -0,0 +1,6 @@
+ 
+for a in range(1000,10000,1):
+    if str(a)==str(a)[::-1]:
+        print(a)
+        
+        
