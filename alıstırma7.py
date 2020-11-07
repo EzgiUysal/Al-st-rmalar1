942afca9b9d54a9827401e0ca43707aa13e0a887
EzgiUysal <57458821+EzgiUysal@users.noreply.github.com>
2020-11-07 16:35:58 +0300

    Add files via upload

---
+++ alıstırma7.py
@@ -0,0 +1,13 @@
+ezgi = 0
+for i in range(100,1000):
+    i = str(i)
+    if int(i[0]) + int(i[1]) == int(i[2]) :
+        print(i)
+        ezgi += 1
+print("sum of numbers: " , ezgi)
+        
+        
+        
+        
+        
+        
